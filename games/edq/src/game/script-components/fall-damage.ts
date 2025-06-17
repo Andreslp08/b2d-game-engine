0b069f524fdecca20845c1c83625f386a302c6b6
Andrés López <andreslp08@hotmail.com>
2025-06-17 18:12:20 -0500

    corrección de problema de daño

--- games/edq/src/game/script-components/fall-damage.ts
+++ games/edq/src/game/script-components/fall-damage.ts
@@ -2,11 +2,15 @@ import { GameObject } from "engine/common/entities/game-object";
 import { ScriptComponent } from "engine/scripts/script-component";
 import { HealthComponent } from "./health-component";
 import { DynamicBody } from "engine/physics/components/dynamic-body";
+import { Transform } from "engine/common/components/transform";
 
 export class FallDamage extends ScriptComponent {
 	private wasOnGround: boolean = false;
 	private startFallPosition = 0;
-	onStart(): void {}
+	onStart(): void {
+		const transform = this.entity.getComponent(Transform);
+		this.startFallPosition = transform.position.y;
+	}
 
 	onUpdate(): void {
 		const gameObject = this.entity as GameObject;
@@ -18,6 +22,7 @@ export class FallDamage extends ScriptComponent {
 		const isOnground = dynamicBody.isOnGround;
 		if (isOnground && this.wasOnGround == false) {
 			const fallDistance = Math.abs(this.startFallPosition - currentPosition.y);
+			console.log('fall distance',fallDistance);
 			if (fallDistance > 6) {
 				const damage = fallDistance * 2.5;
 				healthComponent.setHealth(healthComponent.getHealth() - damage);
