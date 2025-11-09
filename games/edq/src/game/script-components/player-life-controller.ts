09a6d311dc267ef41bd4c4ca6756d8ee21133dbe
Andrés López <andreslp08@hotmail.com>
2025-11-08 20:30:59 -0500

    control player damage

--- games/edq/src/game/script-components/player-life-controller.ts
+++ games/edq/src/game/script-components/player-life-controller.ts
@@ -9,12 +9,16 @@ import {
 import { MathUtil } from "engine/math/math-util";
 import { Time } from "engine/common/interfaces/time";
 import { PlayerController } from "./player-controller";
+import { ShieldComponent } from "./shield-component";
 
 export class PlayerLifeController extends ScriptComponent {
 	private grayscaleValue: number = 0;
 	private fov: number = 1;
 	private cameraShakeTimer: number = 0;
-
+	prevHealth = -1;
+	prevShield = -1;
+	private disableInputStartTime = 0;
+	private disableInputDuration = 0.3;
 	onStart(): void {
 		this.grayscaleValue = 0;
 		this.fov = 1;
@@ -35,6 +39,55 @@ export class PlayerLifeController extends ScriptComponent {
 		EffectsCameras.currentCamera.setRotation(rotation);
 	}
 
+	setDamage(damage: number) {
+		const entity = this.getEntity();
+		if (!entity) return;
+		const shieldComponent = entity.getComponent(ShieldComponent);
+		const healthComponent = entity.getComponent(HealthComponent);
+		if (shieldComponent && healthComponent) {
+			if (shieldComponent.getShield() > 0) {
+				shieldComponent.setDamage(damage);
+			} else {
+				healthComponent.setDamage(damage);
+			}
+		} else if (shieldComponent && !healthComponent) {
+			shieldComponent.setDamage(damage);
+		} else if (!shieldComponent && healthComponent) {
+			healthComponent.setDamage(damage);
+		}
+	}
+
+	handleDamageCoolDown() {
+		const entity = this.entity;
+		const playerController = entity.getComponent(PlayerController);
+		if (!playerController) return;
+
+		const healthC = entity.getComponent(HealthComponent);
+		const health = healthC.getHealth();
+		const shieldC = entity.getComponent(ShieldComponent);
+		const shield = shieldC.getShield();
+
+		if (this.prevHealth !== -1 && this.prevHealth > health) {
+			playerController.enableInputController = false;
+			this.disableInputStartTime = Time.time;
+		}
+
+		if (this.prevShield !== -1 && this.prevShield > shield) {
+			playerController.enableInputController = false;
+			this.disableInputStartTime = Time.time;
+		}
+
+		if (!playerController.enableInputController) {
+			const elapsed = Time.time - this.disableInputStartTime;
+			if (elapsed >= this.disableInputDuration) {
+				playerController.enableInputController = true;
+			}
+		}
+
+		this.prevHealth = health;
+		this.prevShield = shield;
+	}
+
 	onUpdate(): void {
 		const entity = this.getEntity();
 		if (!entity) return;
@@ -46,14 +99,17 @@ export class PlayerLifeController extends ScriptComponent {
 		if (!scene) return;
 
 		if (health.getHealth() <= 0) {
-							this.cameraShakeTimer += Time.unscaledDeltaTime;
-				const shakeSpeed = 1.5; // Oscilaciones por segundo
-				const shakeAmount = 5; // Rango de movimiento (radianes)
+			const playerController = entity.getComponent(PlayerController);
+			playerController.enabled = false;
 
-				const shake =
-					Math.sin((this.cameraShakeTimer * shakeSpeed * Math.PI) / 2) * shakeAmount;
+			this.cameraShakeTimer += Time.unscaledDeltaTime;
+			const shakeSpeed = 1.5; // Oscilaciones por segundo
+			const shakeAmount = 5; // Rango de movimiento (radianes)
 
-				this.changeAllCamerasRotation(shake);
+			const shake =
+				Math.sin((this.cameraShakeTimer * shakeSpeed * Math.PI) / 2) * shakeAmount;
+
+			this.changeAllCamerasRotation(shake);
 			this.grayscaleValue = MathUtil.lerp(
 				this.grayscaleValue,
 				100,
@@ -69,16 +125,12 @@ export class PlayerLifeController extends ScriptComponent {
 				Time.timeScale = newTimeScale < 0.01 ? 0 : newTimeScale;
 			}
 
-            			// FOV zoom suave
+			// FOV zoom suave
 			if (this.fov <= 3 && this.grayscaleValue >= 20) {
 				this.fov += 0.3 * Time.unscaledDeltaTime;
 				this.changeAllCamerasFOV(this.fov);
 			}
-
-			const playerController = entity.getComponent(PlayerController);
-			if (playerController) {
-				entity.deleteallComponentsByClass(PlayerController);
-			}
 		}
+		this.handleDamageCoolDown();
 	}
 }
