7bd7ef1fb1d0f3f73a514bbea013ffb32d27271d
Andrés López <andreslp08@hotmail.com>
2026-09-19 22:27:31 -0500

    Update index.tsx

--- games/edq/src/ui/general-layers/index.tsx
+++ games/edq/src/ui/general-layers/index.tsx
@@ -1,10 +1,10 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useGameStore } from "../../store/store";
-import { LoadingScreen } from "./loading-screen";
-import { MainMenu } from "./main-menu";
+import { GENERAL_VIEWS_ROUTER } from "./router";
 export const GeneralLayer = () => {
 	const currentUI = useGameStore((state) => state.currentUI);
 	const activeLayer = currentUI?.scope === "global" ? currentUI.layer : null;
+	const ActiveView = activeLayer ? GENERAL_VIEWS_ROUTER[activeLayer] : null;
 
 	return (
 		<motion.div
@@ -15,30 +15,17 @@ export const GeneralLayer = () => {
 			transition={{ duration: 0.3 }}
 			className="w-full h-screen flex items-center justify-center"
 		>
-			<AnimatePresence mode="wait">
-				{activeLayer === "loadingScreen" && (
+			<AnimatePresence mode="popLayout">
+				{activeLayer && (
 					<motion.div
-						key="loading"
+						key={activeLayer}
 						initial={{ opacity: 0 }}
 						animate={{ opacity: 1 }}
 						exit={{ opacity: 0 }}
 						transition={{ duration: 0.3 }}
 						className="flex justify-center items-center w-full h-full  text-white"
 					>
-						<LoadingScreen />
-					</motion.div>
-				)}
-
-				{activeLayer === "mainMenu" && (
-					<motion.div
-						key="menu"
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						exit={{ opacity: 0 }}
-						transition={{ duration: 0.3 }}
-						className="flex justify-center items-center w-full h-full"
-					>
-						<MainMenu />
+						<ActiveView />
 					</motion.div>
 				)}
 			</AnimatePresence>
